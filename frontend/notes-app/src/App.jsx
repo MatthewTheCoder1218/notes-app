4688326695ed5f818a07f67e44a8e31b333589cd
Lightspeed code <matthewmicheal2023@gmail.com>
2024-12-25 10:08:22 +0100

    update

--- frontend/notes-app/src/App.jsx
+++ frontend/notes-app/src/App.jsx
@@ -1,5 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
 import Signup from "./pages/Signup/Signup";
@@ -7,6 +12,7 @@ import Signup from "./pages/Signup/Signup";
 const routes = (
   <Router>
     <Routes>
+      <Route path="/" element={<Navigate to="/login" />} />
       <Route path="/dashboard" exact element={<Home />} />
       <Route path="/login" exact element={<Login />} />
       <Route path="/signup" exact element={<Signup />} />
