2b9fe618b47611cb7197e35b2981668bf03ac413
Lightspeed code <matthewmicheal2023@gmail.com>
2024-12-27 18:59:42 +0100

    my update

--- frontend/notes-app/src/components/cards/ProfileInfo.jsx
+++ frontend/notes-app/src/components/cards/ProfileInfo.jsx
@@ -4,7 +4,7 @@ import { getInitials } from "../../utils/helper";
 const ProfileInfo = ({ onLogout, userInfo }) => {
   return (
     <div className="flex items-center gap-3">
-      <div className="w-12 h-12 flex items-center justify-center rounded-full text-slate-950 font-sm sm:font-medium bg-slate-100">
+      <div className="w-12 h-12 hidden sm:flex items-center justify-center rounded-full text-slate-950 font-medium bg-slate-100">
         {getInitials(userInfo?.fullName)}
       </div>
 
