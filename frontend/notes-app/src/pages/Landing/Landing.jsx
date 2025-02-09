e91877781fda705604523b73c24bb7730d826bf6
MatthewTHeCoder1218 <matthewmicheal2023@gmail.com>
2025-02-09 13:21:51 +0100

    adding of elements

--- frontend/notes-app/src/pages/Landing/Landing.jsx
+++ frontend/notes-app/src/pages/Landing/Landing.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import Image from "../../assets/images/image.png";
 
 const Landing = () => {
   return (
     <div>
-      <p>This is the landing page</p>
+      <div>
+        <h1>Your Notes, Organized and Private. Your Productivity, Unleashed</h1>
+        <p>
+          Effortlessly organize your notes, ideas and to-dos in one central
+          place while keeping your data private
+        </p>
+        <button disabled="disabled">Try now for free</button>
+      </div>
+      <img src={Image} alt="" />
     </div>
   );
 };
